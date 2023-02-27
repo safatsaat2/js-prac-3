c85bd31b8aa447945042a18102b7928ee709500f
safatsaat2 <sakibsafat47@gmail.com>
2023-02-27 20:29:34 +0600

    second commit

---
+++ object.js
@@ -0,0 +1,14 @@
+const player = {
+    name : "katol",
+    job: "housewife"
+}
+
+class person{
+    gala = "lala"
+    constructor(age){
+        this.age= age;
+    }
+}
+
+console.log(player);
+console.log(new person(79))
