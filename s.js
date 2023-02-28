ce74d5f08f1fac08edfc514411fc0ecc043ba10a
safatsaat2 <sakibsafat47@gmail.com>
2023-02-28 21:19:44 +0600

    undifien

--- s.js
+++ s.js
@@ -1,6 +1,5 @@
-const isTrue='false'; 
-if(!isTrue){
-console.log('hello');
-} else {
-console.log('world'); 
-}
+function sum(p, q) {
+    p + q;
+   }
+   const result = sum(2, 3);
+   console.log(result);
