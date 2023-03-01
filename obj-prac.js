0c50dd195cc05b9c4ece96fd1051ec9914c9ca80
safatsaat2 <sakibsafat47@gmail.com>
2023-03-01 19:39:30 +0600

    4th prac done

--- obj-prac.js
+++ obj-prac.js
@@ -1,29 +1,36 @@
-const obj = {
-    name : `Mafat`,
-    age : 26,
-    lala :  () => {
-        return console.log(obj.age);
-    },
-    job : [`Roti Maker`, `Floor Cleaner`, `Bathroom cleaner`],
-    address : {
-        city :  `Sylhet`,
-        country : `Bangladesh`,
-    },
+// const obj = {
+//     name : `Mafat`,
+//     age : 26,
+//     lala :  () => {
+//         return console.log(obj.age);
+//     },
+//     job : [`Roti Maker`, `Floor Cleaner`, `Bathroom cleaner`],
+//     address : {
+//         city :  `Sylhet`,
+//         country : `Bangladesh`,
+//     },
 
-}
+// }
 
-obj.lala()
+// obj.lala()
 
-const Details = `Hi, My name is ${obj.name}. I live in ${obj.address.city}. I do three kind of job. one of them is ${obj.job[1]}.`;
-console.log(Details);
+// const Details = `Hi, My name is ${obj.name}. I live in ${obj.address.city}. I do three kind of job. one of them is ${obj.job[1]}.`;
+// console.log(Details);
 
-const numm = 89;
 
-const arrowFunction = (num1, num2) =>{
-    let resultNum1 = num1 + 7;
-    const resultNum2 = num2 + 7;
-    const resultFinal = resultNum1 + resultNum2;
-    return console.log(resultFinal)
-}
 
-arrowFunction(1001, 120);
+// const arrowFunction = (num1, num2) =>{
+//     let resultNum1 = num1 + 7;
+//     const resultNum2 = num2 + 7;
+//     const resultFinal = resultNum1 + resultNum2;
+//     return console.log(resultFinal)
+// }
+
+// arrowFunction(1001, 120);
+
+const numArray = [80,20,90,30,40,50];
+const newArray= []
+let bab = numArray.map(num => num % 7)
+
+
+console.log(bab)
