552f0438d285086e0ac66bfcbd66d1a60bdc96bc
safatsaat2 <sakibsafat47@gmail.com>
2023-03-13 23:36:57 +0600

    10th commit

---
+++ condition.js
@@ -0,0 +1,16 @@
+const num = 10;
+const num1 = 52;
+const name1 = "tumar matha"
+if (num < num1){
+    console.log("num1 is big");
+}
+else
+{
+    console.log("num is big")
+}
+if( name1.length > num && name1 === String){
+    console.log('Too big name')
+}{
+
+    console.log('Not so big name')
+}
